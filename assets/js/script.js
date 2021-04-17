8afacc7d59d32daf63881493a27a5c44448ba811
reisnern <nreisne@wgu.edu>
2021-04-17 15:33:23 -0600

    Added Moment and time tracking

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
-//Current = document.querySelector("currentDay")
+//Current = querySelector("currentDay")
 var current =$("currentDay")
 //Start Moment
 var todaymoment = moment().format("dddd, MMMM Do YYYY h:mm:ss a")
 //Create line three 
 current.append(todaymoment)
+//saveBtn = querySelector(".saveBtn")
+var Save = $(".saveBtn")
+//Keep track of time in milliseconds
+var update = setInterval(updateHour, 3600000)
+
